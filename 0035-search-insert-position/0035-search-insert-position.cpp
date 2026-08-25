1ff32d5076f4e4711751929f1da76a4792378c84
Kanishka <kanishka130405@gmail.com>
2026-08-25 16:50:49 +0530

    Time: 0 ms (100%), Space: 13.6 MB (43.35%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+
+    int searchInsert(vector<int>& nums, int target) {
+        bool found=false;
+        for(int i=0 ;i< nums.size(); i++){
+            if(nums[i]==target){
+                return i;
+                found=true;
+            }
+        }
+
+        if(!found){
+            for(int i=0;i<nums.size();i++){
+                if(nums[i]>target){
+                    return i;
+                    break;
+                }
+            }
+        }
+       return nums.size();
+    }
+};
